9720f695b0c3238c0174ebee61c39eff62d7a574
pcfaktor <pcfaktor@gmail.com>
2024-08-16 17:07:18 +0300

    feat: implement delete user endpoint (#56)

--- src/rocket_routes/mod.rs
+++ src/rocket_routes/mod.rs
@@ -119,30 +119,6 @@ impl<'r> FromRequest<'r> for User {
     }
 }
 
-// #[rocket::async_trait]
-// impl<'r> FromData<'r> for UpdateUserDto {
-//     type Error = Value;
-
-//     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> data::Outcome<'r, Self> {
-//         let string = match data
-//             .open(req.limits().get("birth_date").unwrap_or(256.bytes()))
-//             .into_string()
-//             .await
-//         {
-//             Ok(string) if string.is_complete() => string.into_inner(),
-//             _ => return Outcome::Error((Status::PayloadTooLarge, "Date too large")),
-//         };
-
-//         match NaiveDate::parse_from_str(&string, "%Y-%m-%d") {
-//             Ok(date) => Outcome::Success(date),
-//             Err(_) => Outcome::Error((
-//                 Status::UnprocessableEntity,
-//                 ProfileError::InvalidBirthDate.value(),
-//             )),
-//         }
-//     }
-// }
-
 pub async fn get_client_info(client_addr: IpAddr) -> Result<String, Box<dyn std::error::Error>> {
     let date_time = Utc::now().format("%d %B %Y, %H:%M UTC").to_string();
 
